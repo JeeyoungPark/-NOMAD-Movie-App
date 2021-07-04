286b4aacd56a107effa7132483ba1503d36dbbad
JY <jeeyoung0321@gmail.com>
2021-07-04 13:35:20 +0900

    feat: 3.0-3.2 about component life cycle

--- src/App.js
+++ src/App.js
@@ -1,77 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 3.1,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.7,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 2.2,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 3.3,
+// class component는 class이며, react component로부터 확장된다.
+// react는 자동적으로 class component의 render method를 실행한다.
+// state는 object이다.
+// state에는 바꾸고 싶은 data를 넣는다.
+// button에는 onClick이라는 prop이 기본적으로 있다.
+// this.add()에서 ()는 즉시 함수를 호출해라는 것이고,
+// this.add는 click했을 때만 함수를 호출하는 것이다.
+
+//setState를 호출할 때마다 새로운 state로 render메서드를 호출한다.
+
+//life cycle component : 리액트가 component를 생성하는 없애는 방법
+//생성: constructor -> render -> componentDidMount
+//업데이트: setState호출 -> render -> componentDidUpdate
+//죽음: componentWillUnmounting
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log('constructor(): hello');
   }
-];
+  state = {
+    count: 0
+  };
 
-// function renderFood(dish) {
-//   return <Food key={dish.id} name={dish.name} picture={dish.image} />
-// }
-
-function Food ({ name, picture, rating }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name}></img>
-  </div>
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired, //isRequired : 필수
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-
-      {/* {foodILike.map(renderFood)} */}
-    </div>
-  );
+  add = () => {
+    this.setState(current => ({ count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1}));
+  };
+  reset = () => {
+    this.setState({ count: 0});
+  }
+  componentDidMount() {
+    console.log('componentDidMount(): component rendered');
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate(): I just updated");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount(): byebye");
+  }
+  render() {
+    console.log('Render(): Im rendering');
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
 }
 
 export default App;
